5282343624e8add880165c52e7e1d9176deaa3fb
Gumbal117 <GumOsornio@hotmail.com>
2023-11-21 10:36:27 -0600

    Gaming

--- Assets/Scripts/Sistemas/Spawn.cs
+++ Assets/Scripts/Sistemas/Spawn.cs
@@ -9,43 +9,12 @@ public class Spawn : PoolManager
     public List <Transform> enemys;
     [SerializeField] private GameObject enemigos;
     [SerializeField] public float tiempoEnemigos;
-    [SerializeField] public int cantidadDeEnemigos;
-    [SerializeField] public int cantidadDeEnemigosEnJuego;
-    [SerializeField] public bool esInfinito;
     private float tiempoSigienteEnemigo;
-    public Quaternion rotacion;
-    private void Start()
-    {
-        
-
-    }
+    
+   
     void Update()
     {
-        if (esInfinito)
-        {
-            JuegoInfinito();
-        }
-        else
-        {
-            JuegoNormal();
-        }
-
-    }
-
-    
-    public void JuegoNormal()
-    {
-        tiempoSigienteEnemigo += Time.deltaTime;
-        if (tiempoSigienteEnemigo >= tiempoEnemigos)
-        {
-            PedirObjeto();
-            tiempoSigienteEnemigo = 0;
-            cantidadDeEnemigosEnJuego++;
-        }
-        if (cantidadDeEnemigos == cantidadDeEnemigosEnJuego)
-        {
-            gameObject.SetActive(false);
-        }
+        JuegoInfinito();
     }
     public void JuegoInfinito()
     {
@@ -61,15 +30,14 @@ public class Spawn : PoolManager
             }
 
         }
-        if (cantidadDeEnemigos == cantidadDeEnemigosEnJuego)
-        {
-            gameObject.SetActive(false);
-        }
+       
     }
     public override GameObject PedirObjeto()
     {
         GameObject Objeto = base.PedirObjeto();
-        Objeto.transform.position = transform.position;
+        
+        Vector2 pos= new Vector2 (Random.Range(-50, 50), Random.Range(-50, 50));
+        Objeto.transform.position = pos;
         Objeto.transform.rotation = transform.rotation;
         Objeto.SetActive(true);
         enemys.Add(Objeto.transform);
