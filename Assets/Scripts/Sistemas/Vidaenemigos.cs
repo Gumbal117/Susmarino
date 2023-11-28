3a7e4f9daf31bef5f2910b6c5682b081f6f9a614
AnaRB29 <GumOsornio@hotmail.com>
2023-11-28 11:24:35 -0600

    Final

--- Assets/Scripts/Sistemas/Vidaenemigos.cs
+++ Assets/Scripts/Sistemas/Vidaenemigos.cs
@@ -6,13 +6,13 @@ public class Vidaenemigos : MonoBehaviour
 {
     [SerializeField] private float vida;
     [SerializeField] private float vidaMáxima = 3;
-    // Start is called before the first frame update
-    void Start()
+    
+    
+
+    private void OnEnable()
     {
         vida = vidaMáxima;
     }
-
-    // Update is called once per frame
     void Update()
     {
         if (vida <= 0)
